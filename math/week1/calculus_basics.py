87850a62ccc9c8dbc54f31b6a529f6328a5a26db
arocha2017 <arocha2017@gmail.com>
2026-02-27 15:57:10 +0500

    Add calculus basics exercises file

---
+++ math/week1/calculus_basics.py
@@ -0,0 +1,19 @@
+# Calculus Basics Exercises
+
+## Exercise 1: Limits
+
+Evaluate the following limits:
+1. \(\lim_{x \to 0} \frac{sin(x)}{x}\)
+2. \(\lim_{x \to \infty} \frac{1}{x}\)
+
+## Exercise 2: Derivatives
+
+Find the derivative of the following functions:
+1. \(f(x) = x^2 + 3x + 5\)
+2. \(g(x) = \sin(x)\)
+
+## Exercise 3: Integrals
+
+Calculate the following integrals:
+1. \(\int (3x^2 + 2x + 1)dx\)
+2. \(\int \sin(x)dx\)
